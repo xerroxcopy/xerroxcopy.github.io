9eb9eec5124b1d1d21afda7aff14c4905cf87974
xerroxcopy@gmail.com <frail.aenima@gmail.com>
2018-03-13 14:18:24 +0900

    ebgaramond, huge changes

--- js/faust.js
+++ js/faust.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-
-	$('.bottom').velocity({'opacity':'1'}, 1000);
-	var intros = [
-					'MANY YEARS OF BLESSING SHINE ON THIS PAIR; WITH BONDS HOW TENDER, HOW TOUCHINGLY THEY INTERTWINE',
-					'I hope we’ll get along together: to drive away the gloomy weather.',
-					'Now I take you into my keeping, a welcome, and silent guest.',
-					'I seem exhausted, yet created new, Enmeshed with you, the unknown and the true.'
-					]
-
-	selectquote = Math.floor(Math.random()*3) + 0;
-	$('.text p').text(intros[selectquote]);
-
-	$('.text p').each(function(){
-	    var text = $(this).html().split(' '),
-	        len = text.length,
-	        result = []; 
-
-	    for( var i = 0; i < len; i++ ) {
-	        result[i] = '<span class="word">' + text[i] + '</span>';
-	    }
-	    $(this).html(result.join(' '));
-		});
-
-	setTimeout(function() { 
-
-
-		$('.word').each(function() {
-						var $this = $(this);
-
-						setTimeout(function(){ 
-							$this.addClass('visible');
-						}, Math.floor(Math.random()*1500) + 100);
-		});
-
-		$('.word:contains("Faust")').each(function() {
-			$(this).addClass('faust');
-		});
-
-	}, 10);
-
-/*	$('body').on('click', function() {
-			$('.word').each(function() {
-					var $this = $(this);
-
-					setTimeout(function(){ 
-						$this.addClass('fadeOut');
-					}, Math.floor(Math.random()*3500) + 0);
-				  }); 
-		   
-
-		   setTimeout(function() {
-		   	$('#contact').fadeIn(300);
-		   }, 3500);
-	}); */
+//
+//	$('.bottom').velocity({'opacity':'1'}, 1000);
+//	var intros = [
+//					'MANY YEARS OF BLESSING SHINE ON THIS PAIR; WITH BONDS HOW TENDER, HOW TOUCHINGLY THEY INTERTWINE',
+//					'I hope we’ll get along together: to drive away the gloomy weather.',
+//					'Now I take you into my keeping, a welcome, and silent guest.',
+//					'I seem exhausted, yet created new, Enmeshed with you, the unknown and the true.'
+//					]
+//
+//	selectquote = Math.floor(Math.random()*3) + 0;
+//	$('.text p').text(intros[selectquote]);
+//
+//	$('.text p').each(function(){
+//	    var text = $(this).html().split(' '),
+//	        len = text.length,
+//	        result = []; 
+//
+//	    for( var i = 0; i < len; i++ ) {
+//	        result[i] = '<span class="word">' + text[i] + '</span>';
+//	    }
+//	    $(this).html(result.join(' '));
+//		});
+//
+//	setTimeout(function() { 
+//
+//
+//		$('.word').each(function() {
+//						var $this = $(this);
+//
+//						setTimeout(function(){ 
+//							$this.addClass('visible');
+//						}, Math.floor(Math.random()*1500) + 100);
+//		});
+//
+//		$('.word:contains("Faust")').each(function() {
+//			$(this).addClass('faust');
+//		});
+//
+//	}, 10);
+//
+///*	$('body').on('click', function() {
+//			$('.word').each(function() {
+//					var $this = $(this);
+//
+//					setTimeout(function(){ 
+//						$this.addClass('fadeOut');
+//					}, Math.floor(Math.random()*3500) + 0);
+//				  }); 
+//		   
+//
+//		   setTimeout(function() {
+//		   	$('#contact').fadeIn(300);
+//		   }, 3500);
+//	}); */
 
 	$('.next').click(function(e) {
 
@@ -126,8 +126,11 @@ $(document).ready(function() {
 		opens = $(this).attr('data-opens');
 		console.log(opens);
 		target = $('.hidden[data-opened-by='+opens+']');
+		console.log('targeted');
 		$(this).addClass('clicked');
+		console.log('clicked');
 		target.show();
+		console.log('showed');
 		setTimeout(function() {
 			target.addClass('visible');
 		}, 100);
