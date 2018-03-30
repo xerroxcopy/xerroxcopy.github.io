5032a140485efa9cf2c1f74c1815f8114b72ae48
xerroxcopy@gmail.com <frail.aenima@gmail.com>
2018-03-31 08:41:31 +0900

    jquery modification, education

--- js/faust.js
+++ js/faust.js
@@ -1,141 +1,38 @@
 $(document).ready(function() {
-//
-//	$('.bottom').velocity({'opacity':'1'}, 1000);
-//	var intros = [
-//					'MANY YEARS OF BLESSING SHINE ON THIS PAIR; WITH BONDS HOW TENDER, HOW TOUCHINGLY THEY INTERTWINE',
-//					'I hope we’ll get along together: to drive away the gloomy weather.',
-//					'Now I take you into my keeping, a welcome, and silent guest.',
-//					'I seem exhausted, yet created new, Enmeshed with you, the unknown and the true.'
-//					]
-//
-//	selectquote = Math.floor(Math.random()*3) + 0;
-//	$('.text p').text(intros[selectquote]);
-//
-//	$('.text p').each(function(){
-//	    var text = $(this).html().split(' '),
-//	        len = text.length,
-//	        result = [];
-//
-//	    for( var i = 0; i < len; i++ ) {
-//	        result[i] = '<span class="word">' + text[i] + '</span>';
-//	    }
-//	    $(this).html(result.join(' '));
-//		});
-//
-//	setTimeout(function() {
-//
-//
-//		$('.word').each(function() {
-//						var $this = $(this);
-//
-//						setTimeout(function(){
-//							$this.addClass('visible');
-//						}, Math.floor(Math.random()*1500) + 100);
-//		});
-//
-//		$('.word:contains("Faust")').each(function() {
-//			$(this).addClass('faust');
-//		});
-//
-//	}, 10);
-//
-///*	$('body').on('click', function() {
-//			$('.word').each(function() {
-//					var $this = $(this);
-//
-//					setTimeout(function(){
-//						$this.addClass('fadeOut');
-//					}, Math.floor(Math.random()*3500) + 0);
-//				  });
-//
-//
-//		   setTimeout(function() {
-//		   	$('#contact').fadeIn(300);
-//		   }, 3500);
-//	}); */
 
-	$('.next').click(function(e) {
+  $('a.open').on('click', function() {
+    opens = $(this).attr('for');
+    console.log(opens);
+    target = $('.hidden[id=' + opens + ']');
+    $(this).addClass('clicked');
+    target.show();
+    setTimeout(function() {
+      target.addClass('visible');
+    }, 100);
+  })
 
-		$('#intro').addClass('noshadow');
-		e.preventDefault();
-		nextelem = $(this).attr('data-next');
-		next = $('#'+nextelem);
+  $('a.close').on('click', function() {
+    closes = $(this).attr('for');
+    console.log(closes);
+    target = $('.shown[hideid=' + closes + ']');
+    $(this).addClass('clicked'); // might affect in bad way?
+    target.show();
+    setTimeout(function() {
+      target.addClass('hidden');
+    }, 100);
+  })
 
-		if (nextelem = 1) {
-			setTimeout(function() {
-				$('.bottom').addClass('active');
-			}, 1200);
-			$('.top').fadeIn(500);
-		}
+  $('a.hide').on('click', function() {
+    // setTimeout(function() {
 
-		prev = $(this).parent();
-
-		left = $(this).attr('data-left');
-		right = $(this).attr('data-right');
-		console.log(left, right);
-
-		fadeOutChapter($(this).parent());
-		fadeInChapter(next);
-
-		$('body').velocity({backgroundColor:"#fff"}, {duration: 1500});
-
-		$('.half.left').velocity({
-			backgroundColor:left,
-		}, {
-			duration:2500
-		});
-
-		$('.half.right').velocity({
-			backgroundColor:right,
-		}, {
-			duration: 2500
-		});
-
-
-	});
-
-	function fadeInChapter(next) {
-
-		setTimeout(function() {
-			next.show().velocity({
-			opacity:"1"
-			}, {
-				duration:750
-			});
-
-		}, 750);
-
-	}
-
-	function fadeOutChapter(current) {
-
-		current.velocity({
-			opacity:"0",
-		}, {
-			duration:750
-		});
-
-		setTimeout(function() {
-			current.hide();
-		}, 1500);
-
-
-	}
-
-	$('a.toggle').on('click', function() {
-		opens = $(this).attr('for');
-		console.log(opens);
-		target = $('.hidden[id='+opens+']');
-		console.log('targeted');
-		$(this).addClass('clicked');
-		console.log('clicked');
-		target.show();
-		console.log('showed');
-		setTimeout(function() {
-			target.addClass('visible');
-		}, 100);
-
-	})
-
-
+    // $(this).addClass('hidden'); // docchika?
+		$(this).hide();
+    $(this).removeClass('visible'); // docchika shika kaicha dame nara kocchi nokosu
+    // }, 100);
+  })
+  // $('a.longer').on('click', function()) {
+  // 	lengthen = $(this).attr('for');
+  // 	console.log(lengthen);
+  // 	target = $('.hidden[]')
+  //
 });
